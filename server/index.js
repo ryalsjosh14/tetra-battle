dc583a803e85aefa7cfd26a5e0b30a624c481560
Kyle Ginter <kginter@ufl.edu>
2021-09-16 22:43:28 -0400

    Express server + basic networking

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const path = require('path'), //dependencies
+    express = require('express'),
+    morgan = require('morgan'),
+    express_ws = require('express-ws');
+
+const port = 8000;
+const app = express();
+const ws = express_ws(app);
+app.use(morgan('dev')); //wrapping express
+
+app.ws('/', (ws, req) => { //intial connection
+    console.log('Websocket connected');
+    ws.send(JSON.stringify({data: 'Test server message'})); //can use this to send to clients
+    //when message recieved from one player, immediately transmit using above cmd to mapped other player
+    //need to figure out where/how to store map of connections 
+    //(i.e. here for ease of access or on the host of the game's side, and include it as a field in the message passed)
+
+    ws.on('message', (msg) => console.log(msg));
+
+    ws.on('close', () => console.log('Websocket disconnected'));
+  });
+
+
+app.listen(port, () => console.log('Listening on: http://localhost:' + port + '/'));
--- tetribattle/src/App.js
+++ tetribattle/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import { w3cwebsocket } from 'websocket';
 import './App.css';
+
+//currently fails if server is offline before client connects, but that should not matter since clients will be connecting to server intermittently ->
+//and not the other way around: server always running, clients not.
+
+const client = new w3cwebsocket('ws://localhost:8000'); //all of this does not belong here, but is staying here for now 
+const UID = Math.floor(Math.random() * 100); 
+
+client.onopen = () => console.log("Connected to WebSocket");
+client.onmessage = (msg) => console.log(msg.data);
+function send() {
+  client.readyState === 1 ? client.send(JSON.stringify({user: UID, data: 'Test client message'})) : console.log("Server not running");
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={send}>Send message</button>
       </header>
     </div>
   );
